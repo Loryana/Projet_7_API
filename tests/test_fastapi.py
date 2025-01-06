6bbd36d8cdc4f21d63e80ff4d20d907402c0d376
Rémi Treilhaud <loryana9@gmail.com>
2025-01-06 11:32:13 +0100

    test1

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -11,8 +11,7 @@ class TestFastAPI(unittest.TestCase):
     def load_data_test(self):
         #Charger les données nécessaires pour le test
         df = pd.read_csv('test_api.csv', index_col = 0)
-        input_data = df.to_dict()
-        return input_data
+        return df
 
     def test_root_endpoint(self):
         # Tester la route GET /
@@ -22,7 +21,9 @@ class TestFastAPI(unittest.TestCase):
 
     def test_predict_endpoint_success(self):
         # Tester la route POST /predict/ avec des données valides
-        input_data = self.load_data_test()
+        df = self.load_data_test()
+        ligne = df.iloc[0]
+        input_data = ligne.to_dict()
 
         response = self.client.post("/predict/", json=input_data)
         self.assertEqual(response.status_code, 200)
@@ -31,7 +32,9 @@ class TestFastAPI(unittest.TestCase):
 
     def test_predict_endpoint_invalid_data(self):
         # Tester la route POST /predict/ avec des données invalides
-        input_data = self.load_data_test()
+        df = self.load_data_test()
+        ligne = df.iloc[0]
+        input_data = ligne.to_dict()
 
         first_feature = next(iter(input_data.keys()))
         input_data[first_feature] = "invalid"
@@ -41,7 +44,9 @@ class TestFastAPI(unittest.TestCase):
 
     def test_predict_endpoint_missing_data(self):
         # Tester la route POST /predict/ avec des données manquantes
-        input_data = self.load_data_test()
+        df = self.load_data_test()
+        ligne = df.iloc[0]
+        input_data = ligne.to_dict()
     
         first_feature = next(iter(input_data.keys()))
         input_data.pop(first_feature)

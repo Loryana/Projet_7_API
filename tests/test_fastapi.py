8e0853397a7bf071fd1e94da7c935a25ce6aa518
Rémi Treilhaud <loryana9@gmail.com>
2025-01-06 11:54:29 +0100

    test5

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -37,7 +37,7 @@ class TestFastAPI(unittest.TestCase):
         ligne = df.iloc[0]
         input_data = ligne.to_dict()
 
-        first_feature = 'CODE_GENDER'
+        first_feature = next(iter(payload.keys()))
         input_data[first_feature] = "invalid"
 
         response = self.client.post("/predict/", json=input_data)
@@ -49,7 +49,7 @@ class TestFastAPI(unittest.TestCase):
         ligne = df.iloc[0]
         input_data = ligne.to_dict()
     
-        first_feature = 'CODE_GENDER'
+        first_feature = next(iter(payload.keys()))
         input_data.pop(first_feature)
 
         response = self.client.post("/predict/", json=input_data)
